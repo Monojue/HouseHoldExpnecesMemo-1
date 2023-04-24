f74f473ce772a2c179af70260160fcc5f2174458
Kaung Myat Thet <monojue.km@gmail.com>
2023-04-24 22:09:55 +0630

    getAll

--- src/main/java/com/se/controller/ExpenseController.java
+++ src/main/java/com/se/controller/ExpenseController.java
@@ -1,5 +1,6 @@
 package com.se.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,15 +9,18 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.se.entity.Expense;
+import com.se.service.ExpenseService;
 
 @Controller
 public class ExpenseController {
 
-
-
+	@Autowired
+	ExpenseService expenseService;
+	
 	@GetMapping(value = "/")
-	public String home() {
-		return "index";
+	public String home(Model model) {
+		model.addAttribute("list", this.expenseService.getAll());
+		return "home";
 	}
 
 	@GetMapping(value = "/add")
--- src/main/java/com/se/service/ExpenseService.java
+++ src/main/java/com/se/service/ExpenseService.java
@@ -1,5 +1,29 @@
 package com.se.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.se.entity.Expense;
+import com.se.repository.ExpenseRepository;
+
+@Service
 public class ExpenseService {
 
+	@Autowired
+	ExpenseRepository expenseRepository;
+	
+	public List<Expense> getAll(){
+		List<Expense> list = new ArrayList<>();
+		list = this.expenseRepository.findAll();
+		return list;
+	}
+	
+//	public List<Expense> getMontlyList(){
+//		List<Expense> list = new ArrayList<>();
+//		list = this.expenseRepository.getMontlyList();
+//		return list;
+//	}
 }
